fed623271e7f32862a9012ed7e859ebd4c731dfd
Isaac Bernard Vital <isaacbvital@gmail.com>
2026-01-30 16:07:34 -0500

    add image as bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-import Image from "next/image";
 import FormComponent from "./components/formComponent";
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-start justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex w-full  h-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="font-sans text-2xl my-20"> Welcome to  Test.com </h1>
+
+    <div className="flex min-h-screen items-start justify-center bg-zinc-50 font-sans dark:bg-black" style={{
+      backgroundImage: `url('/books_image.jpg')`,
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      width: "100vw",
+      height: "100vh",
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <main className="flex w-full h-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="font-sans text-2xl my-20"> Books.com </h1>
         <FormComponent />
       </main>
     </div>
